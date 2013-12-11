8e762055d96f54632a8530dc3fc3712dbcad2024
Matthew Francis-Landau <matthew@matthewfl.com>
2013-12-10 19:11:33 -0800

    only route53 when not solo

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,22 +9,26 @@
 
 include_recipe 'route53'
 
-[
- node.name,
- "ip-#{node[:ipaddress].gsub('.', '-')}",
- node['ec2']['instance_id'],
-].each do |name|
+unless Chef::Config[:solo]
+  [
+   node.name,
+   "ip-#{node[:ipaddress].gsub('.', '-')}",
+   node['ec2']['instance_id'],
+  ].each do |name|
 
-  route53_record "#{name}.vandelay.io" do
+    route53_record "#{name}.vandelay.io" do
 
-    value node[:ipaddress]
-    type  'A'
+      value node[:ipaddress]
+      type  'A'
+      ttl   60
 
-    zone_id               node[:route53][:zone_id]
-    aws_access_key_id     node[:citadel][:access_key_id]
-    aws_secret_access_key node[:citadel][:secret_access_key]
+      zone_id               node[:route53][:zone_id]
+      aws_access_key_id     node[:citadel][:access_key_id]
+      aws_secret_access_key node[:citadel][:secret_access_key]
 
-    action :create
+      action :create
+    end
+
   end
 
 end
