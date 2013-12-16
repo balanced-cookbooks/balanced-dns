89839283cfc402e37fbc544b6995a146679eef26
Noah Kantrowitz <noah@coderanger.net>
2013-12-16 12:22:08 -0800

    Clean up recipe code a bit.
    Move the solo guard up to the top and also refuse to run on non-EC2 (it would have just failed later on anyway). Also use citadel for all credential information.

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,34 +18,35 @@
 # limitations under the License.
 #
 
+# Don't run this unless its a Real Server™
+return if Chef::Config[:solo] || !node['ec2']
+
 # need otherwise fog warns
-chef_gem 'unf'
+chef_gem 'unf' do
   options "--no-ri --no-rdoc"
-  action :nothing
-end.run_action(:install)
+end
 
 include_recipe 'route53'
 
-unless Chef::Config[:solo]
-  [
-   node.name,
-   "ip-#{node[:ipaddress].gsub('.', '-')}",
-   node['ec2']['instance_id'],
-  ].each do |name|
+[
+ node.name,
+ "ip-#{node[:ipaddress].gsub('.', '-')}",
+ node['ec2']['instance_id'],
+].each do |name|
 
-    route53_record "#{name}.vandelay.io" do
+  route53_record "#{name}.vandelay.io" do
 
-      value node[:ipaddress]
-      type  'A'
-      ttl   60
+    value node['ipaddress']
+    type 'A'
+    ttl 60
 
-      zone_id               node[:route53][:zone_id]
-      aws_access_key_id     node[:citadel][:access_key_id]
-      aws_secret_access_key node[:citadel][:secret_access_key]
-      aws_session_token     citadel.token
+    zone_id node[:route53][:zone_id]
+    aws_access_key_id citadel.access_key_id
+    aws_secret_access_key citadel.secret_access_key
+    aws_session_token citadel.token
 
-      action :create
-    end
-
+    action :create
   end
+
 end
+
