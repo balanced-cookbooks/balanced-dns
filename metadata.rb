12adb40fb0af3eabbf2a4c50e577e07cb4a8ac86
bninja <andrew@poundpay.com>
2013-12-16 03:40:25 -0800

    @coderanger thx, forgot to chkin

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'matthew@balancedpayments.com'
 license          'All rights reserved'
 description      'Installs/Configures balanced-hostname'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.2'
 
 
-depends          'route53', '~> 0.3.1'
+depends          'route53', '~> 0.4.0'
+
+depends          'balanced-citadel', '~> 0.1.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,24 +7,33 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# need otherwise fog warns
+chef_gem 'unf'
+  options "--no-ri --no-rdoc"
+  action :nothing
+end.run_action(:install)
+
 include_recipe 'route53'
 
-[
- node.name,
- "ip-#{node[:ipaddress].gsub('.', '-')}",
- node['ec2']['instance_id'],
-].each do |name|
-
-  route53_record "#{name}.vandelay.io" do
-
-    value node[:ipaddress]
-    type  'A'
-
-    zone_id               node[:route53][:zone_id]
-    aws_access_key_id     node[:citadel][:access_key_id]
-    aws_secret_access_key node[:citadel][:secret_access_key]
-
-    action :create
+if node['ec2']
+  [
+   node.name,
+   "ip-#{node[:ipaddress].gsub('.', '-')}",
+   node['ec2']['instance_id'],
+  ].each do |name|
+  
+    route53_record "#{name}.vandelay.io" do
+  
+      value node[:ipaddress]
+      type  'A'
+  
+      zone_id               node[:route53][:zone_id]
+      aws_access_key_id     citadel.access_key_id
+      aws_secret_access_key citadel.secret_access_key
+      aws_session_token     citadel.token
+  
+      action :create
+    end
+  
   end
-
 end
